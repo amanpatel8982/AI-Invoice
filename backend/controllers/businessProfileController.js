2d4405d9821b478fe604e37ea7655f217c7ab776
Aman Patel <ap7463015@gmail.com>
2026-01-04 15:29:08 +0530

    add business profile model and controller with schema definition

---
+++ backend/controllers/businessProfileController.js
@@ -0,0 +1,5 @@
+import { getAuth } from "@clerk/express";
+import BusinessProfile from "../models/businessProfileModel.js";
+
+
+const API_BASE = 'http://localhost:4000';
---
+++ backend/models/businessProfileModel.js
@@ -0,0 +1,32 @@
+import { sign } from "jsonwebtoken";
+import mongoose from "mongoose";
+import { trim } from "validator";
+
+const businessProfileSchema = new mongoose.Schema({
+    owner:{
+        type: String,
+        required: true,
+        index:true
+    },
+    businessName: { type: String, required: true },
+    email: { type: String, required:false, default: "", trim: true, lowercase: true },
+    address: { type: String, required: false, default: "" },
+    phone: { type: String, required: false, default: "" },
+    gst: { type: String, required: false, default: "" },
+
+    // for images
+    logoUrl: { type: String, required: false, default: null },
+    stampUrl: { type: String, required: false, default: null },
+    signatureUrl: { type: String, required: false, default: null},
+
+    signatureOwnerName: { type: String, required: false, default: "" },
+    signatureOwnerTitle: { type: String, required: false, default: "" },
+
+    defaultTaxPercent: { type: Number, required: false, default: 18 },
+
+},
+{ timestamps: true }
+);
+
+const BusinessProfile = mongoose.model.BusinessProfile || mongoose.model("BusinessProfile", businessProfileSchema);
+export default BusinessProfile;
