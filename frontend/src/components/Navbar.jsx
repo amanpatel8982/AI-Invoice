b73b82625ac19680e305fc136468c03637ebbb3c
Aman Patel <ap7463015@gmail.com>
2026-01-04 23:09:08 +0530

    Refactor Navbar component to use navbarStyles and implement logo link

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { navbarStyles } from '../assets/dummyStyles'
+import Logo from 
+import {Link} from 'react-router-dom';
+
 
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header className={navbarStyles.header}>
+      <div className={navbarStyles.container}>
+        <nav className={navbarStyles.nav}>
+          <div className={navbarStyles.logoSection}>
+          <Link to='/' className={navbarStyles.logoLink}>
+          <img src="{logo}" alt="logo" className={navbarStyles.logoImage} />
+          </Link>
+          </div>
+        </nav>
+      </div>
+    </header>
   )
 }
 
