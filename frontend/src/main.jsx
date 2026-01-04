66a96d0a123420e24a388ca8e94cd25aee3db65d
Aman Patel <ap7463015@gmail.com>
2026-01-04 19:19:22 +0530

    Wrap App component with BrowserRouter for routing support

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { ClerkProvider } from '@clerk/clerk-react'
+import {BrowserRouter} from "react-router-dom";
 
 
  // Import your Publishable Key
@@ -14,8 +15,8 @@ import { ClerkProvider } from '@clerk/clerk-react'
 
 createRoot(document.getElementById('root')).render(
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-     <App />
+      <BrowserRouter>
+       <App />
+      </BrowserRouter>
     </ClerkProvider>
-    
-
-)
+);
